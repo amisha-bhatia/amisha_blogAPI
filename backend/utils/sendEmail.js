98263b7184e235a04d21f3691b11c3375f534118
amisha-bhatia <b.amisha2001@gmail.com>
2026-01-14 15:57:11 +0900

    like and comment feature and basic frontend added

--- backend/utils/sendEmail.js
+++
@@ -1,9 +0,0 @@
-const sendEmail = async (to, subject, html) => {
-  console.log("=== Sending Email ===");
-  console.log("To:", to);
-  console.log("Subject:", subject);
-  console.log("HTML:", html);
-  console.log("====================");
-};
-
-module.exports = sendEmail;
--- frontend/src/api/auth.api.js
+++
@@ -1,4 +0,0 @@
-import API from "./api";
-
-export const login = (data) => API.post("/users/login", data);
-export const register = (data) => API.post("/users/register", data);
